7185b5abfb607208131ca64d6de05cbaf322dbae
viratsaini <viratsaini.vr@gmail.com>
2023-10-06 22:34:49 +0530

    commit001

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -7,7 +7,7 @@ export default class Navbar extends Component {
         <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
           <div className="container-fluid">
             <Link className="navbar-brand" to="/">
-              Navbar
+              NewsMonkey
             </Link>
             <button
               className="navbar-toggler"
